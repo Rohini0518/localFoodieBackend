5ba16fa313798d76dd7d30f5ff06e6ee5512d5e5
Rohini0518 <rohinipolina0518@gmail.com>
2025-05-26 14:37:13 +0530

    updated mongodb atlas

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 const express = require("express");
-const cors= require("cors")
+const cors = require("cors");
 const mongoose = require("mongoose");
-const productRouter=require("./routes/product/productRoute")
-const cartRouter=require("./routes/cart/cartRoute")
+const productRouter = require("./routes/product/productRoute");
+const cartRouter = require("./routes/cart/cartRoute");
 const app = express();
 app.use(express.json());
 const port = process.env.PORT || 4000;
-app.use(cors())
+app.use(cors({
+  origin: 'https://localfoodies.netlify.app', 
+  credentials: true
+}));
 app.listen(port, "localhost", () => {
   console.log(`server is running in ${port}`);
 });
 
 mongoose
-  .connect("mongodb://127.0.0.1/localFoodies")
+  .connect(
+    "mongodb+srv://rohinipolina489:Rohini123@cluster0.f6v9ty4.mongodb.net/localFoodies?retryWrites=true&w=majority&appName=Cluster0"
+  )
   .then(() => {
-    console.log("mongodb is connected");
+    console.log("mongodb atlas is connected");
   })
   .catch((err) => {
     console.log("error occured", err);
@@ -24,5 +29,5 @@ app.get("/", (req, res) => {
   res.send("backend is running ");
 });
 
-app.use("/products",productRouter)
-app.use("/cart",cartRouter)
+app.use("/products", productRouter);
+app.use("/cart", cartRouter);
