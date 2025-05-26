d707c5c295b945d719e894c9cd6e62a5487881e0
Rohini0518 <rohinipolina0518@gmail.com>
2025-05-26 18:53:35 +0530

    remooved localhost

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ app.use(cors({
   origin: 'https://localfoodies.netlify.app', 
   credentials: true
 }));
-app.listen(port, "localhost", () => {
-  console.log(`server is running in ${port}`);
-});
+// app.listen(port, "localhost", () => {
+//   console.log(`server is running in ${port}`);
+// });
 
 mongoose
   .connect(process.env.MONGODB_URL)
